9c80f86889b33aeececf0df24c2f15550249c9f4
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2016-05-20 10:59:22 -0300

    Create  1004 - Produto Simples.c

---
+++ 1004 - Produto Simples.c
@@ -0,0 +1,18 @@
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/1004
+
+# include <stdio.h>
+# include <stdlib.h>
+  
+int main () {
+      
+    int v1, v2, PROD;
+    scanf("%d", &v1);
+    scanf("%d", &v2);
+      
+    PROD = v1 * v2;
+      
+    printf("PROD = %d\n", PROD); 
+      
+    system("PAUSE");
+    return 0;
+}
