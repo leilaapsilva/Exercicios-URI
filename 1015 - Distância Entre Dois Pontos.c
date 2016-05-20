80780fa0ac0ff3c535703bd996dba363b0444fac
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2016-05-20 12:08:20 -0300

    Create  1015 - Distância Entre Dois Pontos.c

---
+++ 1015 - Distância Entre Dois Pontos.c
@@ -0,0 +1,18 @@
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/1015
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	double x1, y1, x2, y2, dist;
+	scanf("%lf %lf", &x1, &y1);
+	scanf("%lf %lf", &x2, &y2);
+	
+	dist = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+	
+	printf("%.4lf\n", dist);
+
+	return 0;
+}
