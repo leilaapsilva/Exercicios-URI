db0c179e791e5a233a9b1412a50601114d2cbbf6
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2016-05-20 11:59:04 -0300

    Create  1003 - Soma Simples.c

---
+++ 1003 - Soma Simples.c
@@ -0,0 +1,17 @@
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/1003
+
+#include <stdio.h>
+   
+int main() {
+   
+    int A, B, SOMA;
+    scanf("%d", &A);
+    scanf("%d", &B);
+      
+    SOMA = A + B;
+      
+    printf("SOMA = %d\n",SOMA);
+      
+   
+    return 0;
+}
