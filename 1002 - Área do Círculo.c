5a5db33106968069b7e9e05685a774deef95a5b1
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2016-05-20 12:13:20 -0300

    Create  1002 - Área do Círculo.c

---
+++ 1002 - Área do Círculo.c
@@ -0,0 +1,19 @@
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/1002
+
+#include <stdio.h>
+
+int main()
+{
+	double raio, area;
+	
+	scanf("%lf", &raio);
+	
+	area = raio * raio * 3.14159;
+	
+	printf("A=%.4lf\n", area);
+	
+	
+	
+	system("PAUSE");
+	return 0;
+}
