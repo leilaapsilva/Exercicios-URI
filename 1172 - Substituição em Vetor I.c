4bde4541a29df7f20fc43cfc71225d09f4b9d761
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2016-05-20 11:05:01 -0300

    Create  1172 - Substituição em Vetor I.c

---
+++ 1172 - Substituição em Vetor I.c
@@ -0,0 +1,22 @@
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/1172
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    int X[10], i;
+    
+    for (i = 0; i <= 9; i++){
+    scanf("%d", &X[i]);
+      if (X[i] == 0 || X[i] < 0){
+               X[i] = 1;
+               
+      }
+      
+    }
+    for (i = 0; i <= 9; i++){
+    printf("X[%d] = %d\n", i, X[i]);
+    }
+ 
+    return 0;
+}
